3ce780a2b6830b873c1b895bbff4a9f504515c59
stijnc97 <stijn1997@live.be>
2017-07-20 09:00:21 +0200

    change port

--- index.js
+++ index.js
@@ -45,5 +45,5 @@ app.get(/\/(.*)/, function(req, res) {
   }
 });
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Node luistert op poort', app.get('port'));
+  console.log('Node luistert op poort ', app.get('port'));
 });
