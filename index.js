1ea1794cba0718841c219e4b7183b415a91c7161
stijnc97 <stijn1997@live.be>
2017-07-21 13:11:47 +0200

    wijziging naar poort 3000 ipv 5000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var pagesFile = require('./data/pages.json');
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static('public'))
 
